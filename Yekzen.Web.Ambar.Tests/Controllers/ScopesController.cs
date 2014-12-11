86d9955ab8935d88ec0820189cc6011aea345aa0
bozkan <baozkan@yahoo.com>
2014-12-11 22:15:37 +0200

    Add scope controller.

---
+++ Yekzen.Web.Ambar.Tests/Controllers/ScopesController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yekzen.Web.Ambar;
+using Yekzen.Web.Ambar.Controllers;
+using Yekzen.Domain.Metadata;
+
+namespace Yekzen.Web.Ambar.Tests.Controllers
+{
+    [TestClass]
+    public class ScopesControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            ScopesController controller = new ScopesController();
+
+            // Act
+            IEnumerable<Scope> result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("value1", result.ElementAt(0));
+            Assert.AreEqual("value2", result.ElementAt(1));
+        }
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+            ScopesController controller = new ScopesController();
+
+            // Act
+            var result = controller.Get("5");
+
+            // Assert
+            Assert.AreEqual("value", result);
+        }
+
+        [TestMethod]
+        public void Post()
+        {
+            // Arrange
+            ScopesController controller = new ScopesController();
+
+            // Act
+            controller.Post(new Scope());
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Put()
+        {
+            // Arrange
+            ScopesController controller = new ScopesController();
+
+            // Act
+            controller.Put("5", new Scope());
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            ScopesController controller = new ScopesController();
+
+            // Act
+            controller.Delete("5");
+
+            // Assert
+        }
+    }
+}
---
+++ Yekzen.Web.Ambar/Controllers/ScopesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Yekzen.Domain.Metadata;
+
+namespace Yekzen.Web.Ambar.Controllers
+{
+    public class ScopesController : ApiController
+    {
+        // GET: api/Scopes
+        public IEnumerable<Scope> Get()
+        {
+            return new [] { new Scope{ Name = "Demo" } };
+        }
+
+        // GET: api/Scopes/5
+        public Scope Get(string id)
+        {
+            return new Scope { Name = "Demo" };
+        }
+
+        // POST: api/Scopes
+        public void Post([FromBody]Scope value)
+        {
+        }
+
+        // PUT: api/Scopes/5
+        public void Put(string id, [FromBody]Scope value)
+        {
+        }
+
+        // DELETE: api/Scopes/5
+        public void Delete(string id)
+        {
+        }
+    }
+}
