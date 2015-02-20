d4ff6e1dd6a6f646bd9ab51e709b8c8909bff4b6
bozkan <baozkan@yahoo.com>
2015-02-20 23:06:02 +0200

    Add repository factory.

---
+++ Yekzen.Data.Ravendb/RavenDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Raven.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yekzen.Data.RavenDb
+{
+    public class RavenDbContext : IUnitOfWork
+    {
+        private readonly IDocumentSession session;
+
+        public RavenDbContext(IDocumentStore store)
+        {
+            this.session = store.OpenSession();
+        }
+
+        public void SaveChanges()
+        {
+            this.session.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            this.session.Dispose();
+        }
+    }
+}
---
+++ Yekzen.Data.Ravendb/RavenDbRepositoryFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yekzen.Data.RavenDb
+{
+    public class RavenDbRepositoryFactory  : IRepositoryFactory
+    {
+        public IRepository<TEntity> Get<TEntity>() where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
